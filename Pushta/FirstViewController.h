b36b4f08ff09a9252256b0086d31af4141f54a22
Marcus Kida <kimar@me.com>
2011-04-23 12:15:18 +0200

    Initial Commit

---
+++ Pushta/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  Pushta
+//
+//  Created by Marcus Kida on 23.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ Pushta/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  Pushta
+//
+//  Created by Marcus Kida on 23.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondViewController : UIViewController {
+    
+}
+
+@end
